be03ff46653950a92fb7c924af5d7b8ce2e3d9af
Alejandro Bahena <alejandro.bahsan@gmail.com>
2022-05-28 21:36:46 -0500

    routesChanges

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 
 // === Dependencies ===
 const express = require('express');
-
+const path = require("path");
 // === Server init ===
 const app = express();
+
 
 // === Server able to accept a "what port to listen on" parameter from the environment ===
 const PORT = process.env.PORT || 3000;
@@ -11,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 // ===  Middleware for parsing JSON and urlencoded form data === 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, './public')));
 
 // === Routes  === 
 require('./routes/apiRoutes')(app);
